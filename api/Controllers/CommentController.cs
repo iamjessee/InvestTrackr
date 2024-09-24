a832c298f226115a6e2240f157831aeb20e20dab
Jesse Bullard <jessebullard1996@gmail.com>
2024-09-24 17:12:50 -0600

    more comment additions

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos;
+using api.Interfaces;
 using api.Mappers;
 using api.Repository;
 using Microsoft.AspNetCore.Mvc;
@@ -14,10 +16,12 @@ namespace api.Controllers
     public class CommentController : ControllerBase
     {
         private readonly ICommentRepository _commentRepository;
-        public CommentController(ICommentRepository commentRepository)
+        private readonly IStockRepository _stockRepository;
+
+        public CommentController(ICommentRepository commentRepository, IStockRepository stockRepository)
         {
             _commentRepository = commentRepository;
-            
+            _stockRepository = stockRepository;
         }
 
         [HttpGet]
@@ -40,7 +44,21 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(comment.ToCommentDto());
+            return 
+            Ok(comment.ToCommentDto());
+        }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto)
+        {
+            if(!await _stockRepository.StockExists(stockId))
+            {
+                return BadRequest("Stock does not exist.");
+            }
+
+            var commentModel = commentDto.ToCommentFromCreate(stockId);
+            await _commentRepository.CreateAsync(commentModel);
+            return CreatedAtAction(nameof(GetById), new {id = commentModel.Id}, commentModel.ToCommentDto());
         }
     }
 }
--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -10,5 +10,6 @@ namespace api.Repository
     {
         Task<List<Comment>> GetAllAsync();
         Task<Comment?> GetByIdAsync(int id);
+        Task<Comment> CreateAsync(Comment commentModel);
     }
 }
