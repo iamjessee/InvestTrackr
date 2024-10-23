9e10340ee0f704bc58d2d18485db8d5a5ad07502
Jesse Bullard <jessebullard1996@gmail.com>
2024-10-22 23:12:37 -0600

    fixed api routing for comments and portfolios

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40d74422e54f6ed65c9a0413360547ceb1ab5fe2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1439499fa05d35d4c14d4e1e937139a64372260")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
