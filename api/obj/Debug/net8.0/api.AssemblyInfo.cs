fa6218564ee16bb4da886f52a43ed11b5e7add2e
Jesse Bullard <jessebullard1996@gmail.com>
2024-11-09 15:41:13 -0700

    merge/pull request and gif update

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7aab46768f775e931fa2b3790dedcd483278aab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+962988403725830aec8b461bb64aeaae4959fe29")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
